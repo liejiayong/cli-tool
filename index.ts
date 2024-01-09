33a3315783db30554f06913b5e971912860c8572
liejiayong <809206619@qq.com>
2024-01-09 15:29:59 +0800

    chork: init

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
+// #!/usr/bin/env node
 
 import * as fs from "node:fs";
 import * as path from "node:path";
@@ -8,3 +9,29 @@ import prompts from "prompts";
 import { red, green, bold } from "kolorist";
 
 import ejs from "ejs";
+
+import * as banners from "./utils/banners";
+
+async function init() {
+  console.log();
+  console.log(
+    process.stdout.isTTY && process.stdout.getColorDepth() > 8 ? banners.gradientBanner : banners.defaultBanner
+  );
+  console.log();
+
+  const cwd = process.cwd();
+  const argv = minimist(process.argv.slice(2), {
+    alias: {
+      typescript: ["ts"],
+      "with-tests": ["tests"],
+      router: ["vue-router"],
+    },
+    string: ["_"],
+    // all arguments are treated as booleans
+    boolean: true,
+  });
+}
+
+init().catch((e) => {
+  console.error(e);
+});
